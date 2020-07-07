08ac8aecaa6a7188d16ae7abb506f9183350190e
AdityaSenthilnathan <66099566+AdityaSenthilnathan@users.noreply.github.com>
2020-07-07 00:59:54 -0700

    Add files via upload

---
+++ FireBase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDnXi4gDHtIDqt-X23jHNUwOj52qtiTPB4",
+    authDomain: "virtual-pet-7c49e.firebaseapp.com",
+    databaseURL: "https://virtual-pet-7c49e.firebaseio.com",
+    projectId: "virtual-pet-7c49e",
+    storageBucket: "virtual-pet-7c49e.appspot.com",
+    messagingSenderId: "577519131323",
+    appId: "1:577519131323:web:d68fbd5c9479e0e77da79e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ sketch.js
@@ -0,0 +1,94 @@
+var dogIMG, dogIMG2, dog, foodS, foodStock, dataBase, Exercise;
+const db = firebase.database().ref();
+
+function preload() {
+  dogIMG = loadImage("images/dogImg.png");
+  dogIMG2 = loadImage("images/dogImg1.png");
+  //dataBase = firebase.database();
+  //db = firebase.database().ref();
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  dog = createSprite(250, 250, 10, 10);
+  dog.addImage(dogIMG);
+  dog.scale = .1;
+  textSize(30);
+  fill("green");
+  stroke(10);
+
+
+}
+
+
+function draw() {
+  background(46, 139, 87);
+  drawSprites();
+
+  //add styles here
+  text("Press UP to Feed Your Dog!!", 80, 30);
+  text("Press DOWN to Restock Your Food!!", 0, 70);
+  foodStock = firebase.database().ref("Food");
+  foodStock.on("value", readStock);
+  console.log(foodS);
+  text("food Avalible: " + foodS, 150, 200);
+  //Exercise = datBase.ref("Exercise");
+  if (keyWentDown(UP_ARROW)) {
+    writeStock(foodS);
+    dog.addImage(dogIMG2);
+
+
+  }
+
+  if (keyWentDown(DOWN_ARROW)) {
+    writeStock2(foodS);
+    dog.addImage(dogIMG);
+
+
+  }
+
+}
+
+function readStock(data) {
+  foodS = data.val();
+}
+function writeStock(x) {
+
+  if (x <= 0) {
+
+    x = 0
+
+  }
+  else {
+
+    x = x - 1
+  }
+  firebase.database().ref('/').update({
+
+    Food: x
+  })
+
+}
+
+function writeStock2(x) {
+  
+  if (x <= 0) {
+
+    x = 0
+
+  }
+  else {
+
+    x = x + 1
+  }
+  firebase.database().ref('/').update({
+
+    Food: x
+  })
+
+}
+
+
+
+
